b0aa01f3c8e46baef1325d807a6d583a7c3cb818
Rimeshsapkota <rimesh@lancemeup.com>
2023-07-21 01:23:03 +0545

    changes in many file

--- src/main/java/com/example/practice/controllers/WebpageController.java
+++ src/main/java/com/example/practice/controllers/WebpageController.java
@@ -1,14 +1,17 @@
 package com.example.practice.controllers;
 
+import com.example.practice.entities.LoginUserDto;
 import com.example.practice.entities.RegisterUserDto;
+import com.example.practice.entities.User;
 import com.example.practice.services.CustomUserDetailService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@AllArgsConstructor
 public class WebpageController {
-
+private CustomUserDetailService service;
     @GetMapping("/")
     public String showFrontPage() {
         return "index";
@@ -17,4 +20,17 @@ public class WebpageController {
     public String showRegisterPage() {
         return "register";
     }
+
+   @PostMapping("/register")
+    public String saveStudent(@ModelAttribute("user") RegisterUserDto registerUserDto) {
+        service.registerUser(registerUserDto.mapToUser());
+        return "save";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("user") LoginUserDto loginUserDto) {
+        service.registerUser(loginUserDto.toUser());
+        return "login";
+    }
+
 }
