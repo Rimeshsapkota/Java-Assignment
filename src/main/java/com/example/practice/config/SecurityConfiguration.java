53510e5746c2cac020d4f7c1d76eb41aa9b60b56
Rimeshsapkota <rimesh@lancemeup.com>
2023-07-20 23:01:24 +0545

    add config controller entities repository service package

---
+++ src/main/java/com/example/practice/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.example.practice.config;
+
+import com.example.practice.entities.Role;
+import lombok.AllArgsConstructor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/users/register").permitAll()
+                .antMatchers("/h2-console/**","/h2-console").permitAll()
+                .antMatchers("/api/users/login").permitAll()
+                .antMatchers("/api/admin/*", "/api/admin/**").hasAuthority(Role.ADMIN.name())
+                .antMatchers("/api/user/*", "/api/user/**").hasAuthority(Role.USER.name())
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login", "/register").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
